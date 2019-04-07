f35ceb23d7dfd903b8571707e358929c074857dc
Ryota Yuasa <170055@jc-21.jp>
2019-04-07 23:55:21 +0900

    add: 入力値が範囲内であるかを判定する機能を追加

--- kadai2/scripts.js
+++ kadai2/scripts.js
@@ -2,11 +2,21 @@ function check(obj) {
     if (!isNumber(obj.value)) {
         alert("整数を入力して下さい。");
     }
+    else if (!rangeOf2And100(obj.value)) {
+        alert("2~100の範囲で入力してください。");
+    }
+    else {
+        alert("Good!");
+    }
 }
 
 function isNumber(val) {
     var regex = new RegExp(/^[0-9]+$/);
 
     return regex.test(val);
+}
+
+function rangeOf2And100(val) {
+    return (val >= 2 && val <= 100);
 }
 
