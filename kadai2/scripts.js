7a3b04079f36d4265db19d4a61657e879eeec79b
Ryota Yuasa <170055@jc-21.jp>
2019-04-08 00:27:19 +0900

    add: 入力値が素数であるかを判定する機能を追加

--- kadai2/scripts.js
+++ kadai2/scripts.js
@@ -5,8 +5,11 @@ function check(obj) {
     else if (!rangeOf2And100(obj.value)) {
         alert("2~100の範囲で入力してください。");
     }
+    else if (!isPrime(obj.value)) {
+        alert("入力された整数は素数ではありません。");
+    }
     else {
-        alert("Good!");
+        alert("入力された整数は素数です。");
     }
 }
 
@@ -18,5 +21,24 @@ function isNumber(val) {
 
 function rangeOf2And100(val) {
     return (val >= 2 && val <= 100);
+}
+
+function isPrime(val) {
+    if (val == 2) {
+        return true;
+    }
+    else if (val % 2 == 0) {
+        return false;
+    }
+
+    var val_sqrt = Math.sqrt(val);
+
+    for (var i = 3; i <= val_sqrt; i += 2) {
+        if (val % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
