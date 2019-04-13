969feea2f6e668b5c4978c95a26ce2bf775d7906
Ryota Yuasa <170055@jc-21.jp>
2019-04-13 15:52:13 +0900

    update: 素数判定の結果の表示先をページ上に変更

--- kadai2/scripts.js
+++ kadai2/scripts.js
@@ -1,16 +1,16 @@
 class InputNumberChecker {
     check(obj) {
         if (!this.isNumber(obj.value)) {
-            alert("整数を入力して下さい。");
+            alert('整数を入力して下さい。');
         }
         else if (!this.isRangeOf2And100(obj.value)) {
-            alert("2~100の範囲で入力してください。");
+            alert('2~100の範囲で入力してください。');
         }
         else if (!this.isPrime(obj.value)) {
-            alert("入力された整数は素数ではありません。");
+            document.getElementById('result').innerHTML = obj.value + 'は素数ではありません。';
         }
         else {
-            alert("入力された整数は素数です。");
+            document.getElementById('result').innerHTML = obj.value + 'は素数です。';
         }
     }
 
