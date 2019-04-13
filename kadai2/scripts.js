788a221236e2b7d25d08b234ad1470d3fdc1f5b9
Ryota Yuasa <170055@jc-21.jp>
2019-04-13 15:16:08 +0900

    update: 複数のメソッドを1つのクラスにまとめた

--- kadai2/scripts.js
+++ kadai2/scripts.js
@@ -1,44 +1,52 @@
-function check(obj) {
-    if (!isNumber(obj.value)) {
-        alert("整数を入力して下さい。");
-    }
-    else if (!rangeOf2And100(obj.value)) {
-        alert("2~100の範囲で入力してください。");
-    }
-    else if (!isPrime(obj.value)) {
-        alert("入力された整数は素数ではありません。");
-    }
-    else {
-        alert("入力された整数は素数です。");
-    }
-}
-
-function isNumber(val) {
-    var regex = new RegExp(/^[0-9]+$/);
-
-    return regex.test(val);
-}
-
-function rangeOf2And100(val) {
-    return (val >= 2 && val <= 100);
-}
-
-function isPrime(val) {
-    if (val == 2) {
-        return true;
-    }
-    else if (val % 2 == 0) {
-        return false;
-    }
-
-    var val_sqrt = Math.sqrt(val);
-
-    for (var i = 3; i <= val_sqrt; i += 2) {
-        if (val % i == 0) {
-            return false;
+class InputNumberChecker {
+    check(obj) {
+        if (!this.isNumber(obj.value)) {
+            alert("整数を入力して下さい。");
+        }
+        else if (!this.rangeOf2And100(obj.value)) {
+            alert("2~100の範囲で入力してください。");
+        }
+        else if (!this.isPrime(obj.value)) {
+            alert("入力された整数は素数ではありません。");
+        }
+        else {
+            alert("入力された整数は素数です。");
         }
     }
 
-    return true;
+    isNumber(val) {
+        var regex = new RegExp(/^[0-9]+$/);
+
+        return regex.test(val);
+    }
+
+    rangeOf2And100(val) {
+        return (val >= 2 && val <= 100);
+    }
+
+    isPrime(val) {
+        if (val == 2) {
+            return true;
+        }
+        else if (val % 2 == 0) {
+            return false;
+        }
+
+        var val_sqrt = Math.sqrt(val);
+
+        for (var i = 3; i <= val_sqrt; i += 2) {
+            if (val % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+
+var onButtonClick = function(number) {
+    var inputNumberChecker = new InputNumberChecker();
+    inputNumberChecker.check(number);
 }
 
